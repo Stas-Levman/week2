825b43eeb892d20353d9c973486042033bfc975d
Stas Levman VM <staslevman@gmail.com>
2022-03-02 17:13:38 -0800

    Renamed feature file and removed not used file

--- passwordValidator.sh
+++ passwordValidator.sh
@@ -4,14 +4,19 @@
 #Criteria for the password are: at least 10 characters long, must contain 1 upper case and 1 lower case letter, and at least one digit.
 #The functions that check the string use regex in order to do so.
 
- 
-pass=$1 #Password string we get from the user argument.
 
+
+argument=$1
+getopts ":f:" option
+path="" #Variable that stores the password file path when using option -f.
+pass="" #Password that will be validated.
 valid=0 #Variable that is changed to 1 if criteria for the password is not met. 
 
 RED='\033[0;31m' #Red color
 GREEN='\033[0;32m' #Green color
 NC='\033[0m' #No Color
+
+
 
 
 #Checks if password does not contain at least 1 digit.
@@ -67,9 +72,75 @@ function checkPasswordStrength {
 }
 
 
-checkPasswordStrength
+
+
+#Checking if the argument accepted is an option or a password string.
+function checkIfOption {
+    if [[ $argument =~ ^\-.$ ]]
+        then
+        printf "it's -f\n"
+        #getopts ":f:" option 
+        if [ $option == "f" ]
+        then
+            path=${OPTARG}
+            pass=$(cat $path 2>/dev/null)
+            return 0
+        fi
+    else
+        #printf "it's not -f\n"
+        pass=$argument
+        return 1
+    fi
+}
+
+#checkIfOption
+
+#If statement that checks for any errors with the flag letter or argument before validating password.
+function checkOptionErrors {
+    if [ $option == ? ]
+    then
+        printf "${RED}Please input the correct option letter, this script can only accept \"-f\" as an option. followed by a path to the file containing the password.${NC}\n"
+        return 1
+    elif [ $option == : ] || [ ! -f "$path" ]
+    then
+        printf "${RED}File could not be found${NC}\n"
+        return 1
+    else
+        #printf "checkd\n"
+        return 0
+    fi
+}
+
+function validatePassword {
+        if checkIfOption
+        then
+            if checkOptionErrors
+            then
+                checkPasswordStrength
+            fi
+        else
+            checkPasswordStrength
+        fi
+}
+
+# function validatePassword {
+#     if [ checkIfOption -a checkOptionErrors ]
+#     then
+#         checkPasswordStrength
+#     fi
+#}
+
+# function validatePassword {
+#         if checkIfOption && checkOptionErrors
+#         then
+#             checkPasswordStrength
+#         else
+#             checkPasswordStrength
+#         fi
+# }
 
 
 
-
+validatePassword
+printf "\nThe option is: $option\nThe # is: $#\nThe OPTARG is: $OPTARG\nThe OPTINT is: $OPTIND\nThe pass is: $argument\nOption or pass: $checkIfOption\n" 
 
